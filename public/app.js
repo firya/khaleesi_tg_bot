8a8a6d50c5da3d510bf2db70cb210670554e8d9b
Maksim Lebedev <firya@bk.ru>
2020-06-25 11:16:29 +0300

    test auto deploy

--- public/app.js
+++ public/app.js
@@ -1,12 +1,2 @@
 import './router.js';
 import './cron.js';
-
-// import { parseDate } from './utils.js';
-// import Report from './report/report.js';
-// var date = '06.06.2020';
-// var report = new Report();
-// report.getLeadMeterAt(parseDate(date)).then(res => console.log(res));
-
-// import PDF from './report/pdf.js';
-// var date = '02.06.2020';
-// PDF.generatePDF(date);
