eea3a047be49b251879a9152fb8313f5a0da19ba
lebedevm <lebedevm@ozon.ru>
2025-02-14 13:03:03 +0300

    add deploy to traefik docker-compose

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+import 'dotenv/config';
 import express from "express";
-import path from "path";
 
 import { khaleesiTelegramBot } from "./telegram/khaleesi.js";
 import { cheemsTelegramBot } from "./telegram/cheems.js";
-
-const __dirname = path.resolve(path.dirname(""));
 
 var app = express();
 
@@ -26,11 +24,15 @@ app.post(`/bot${process.env.KHALEESI_TELEGRAM_TOKEN}`, (req, res) => {
   res.sendStatus(200);
 });
 
+app.get(`/bot${process.env.CHEEMS_TELEGRAM_TOKEN}`, (req, res) => {
+  res.send("Cheems bot");
+});
+
 app.post(`/bot${process.env.CHEEMS_TELEGRAM_TOKEN}`, (req, res) => {
   cheemsTelegramBot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
+app.listen(80, function () {
+  console.log("Example app listening on port 80!");
 });
