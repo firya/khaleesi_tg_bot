dd1dc6f77068d376232c611d4b5298c400827e08
lebedevm <lebedevm@ozon.ru>
2025-02-14 13:45:09 +0300

    update bot webhook

--- dev.js
+++ dev.js
@@ -2,4 +2,4 @@ const ngrockUrl = "https://e4c843acf917.eu.ngrok.io"; // for local use
 export const hostURL =
   process.env.NODE_ENV == "development"
     ? `${ngrockUrl}`
-    : `https://${process.env.VIRTUAL_HOST}`;
+    : `https://${process.env.DOMAIN}`;
